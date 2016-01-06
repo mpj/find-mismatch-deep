80d84a36b39899ce6496e54f76f252d765cf7c8a
Mattias Petter Johansson <mpj@spotify.com>
2016-01-06 12:56:38 +0100

    initial commmit

---
+++ index.js
@@ -0,0 +1,57 @@
+'use strict';
+const isPlainObject = require('is-plain-object')
+const _ = require('lodash')
+
+module.exports = function findMismatchDeep(expected, actual, path = []) {
+  const isBothArrays = Array.isArray(expected) && Array.isArray(actual)
+  const isBothPlainObject = isPlainObject(expected) && isPlainObject(actual)
+
+  if (isBothPlainObject) {
+    // check that all expected patterns match actual
+    const pairsExpected = _.pairs(expected)
+    console.log(pairsExpected)
+    for (var i = 0; i < pairsExpected.length; i++) {
+      const [expectedKey, expectedPatternForKey] = pairsExpected[i]
+
+      const mismatch = findMismatchDeep(
+        expectedPatternForKey,
+        actual[expectedKey],
+        path.concat([expectedKey])
+      )
+
+      if (mismatch) return mismatch;
+    }
+  } else if (isBothArrays) {
+    // check that all array patterns match actual
+    for (var i = 0; i < expected.length; i++) {
+      let matched = false;
+      let firstMismatch = null;
+      for (var j = 0; j < actual.length; j++) {
+        const mismatch = findMismatchDeep(
+          expected[i],
+          actual[j],
+          path.concat([i]))
+
+        if (!mismatch) {
+          matched = true;
+          break;
+        }
+      }
+      if (!matched) {
+        return {
+          path,
+          expected: expected,
+          actual
+        }
+      }
+    }
+
+  } else if (expected !== actual) {
+    return {
+      path,
+      actual,
+      expected
+    }
+  }
+  return null;
+}
---
+++ test.js
@@ -0,0 +1,92 @@
+const test = require('tape')
+const findMismatchDeep = require('.')
+
+
+test('basic case', t => {
+  const pattern = {
+    outer: {
+      inner: 999
+    }
+  }
+  const actual = {
+    outer: {
+      inner: 666
+    }
+  }
+
+  t.deepEqual(findMismatchDeep(pattern, actual), {
+    actual: 666,
+    expected: 999,
+    path: [ 'outer', 'inner' ] }
+  )
+  t.end()
+})
+
+
+test('array matcher', t => {
+  const pattern = {
+    abba: {
+      bertil: [
+        {
+          ceasar: [
+            {
+              dustin: 99
+            }
+          ]
+        },
+      ]
+    }
+  }
+  const actual = {
+    abba: {
+      bertil: [
+        {
+          ceasar: [
+            {
+              dustin: 66,
+              eric: 4
+            },
+          ],
+          curt: 3
+        }
+      ],
+      burt: 2
+    },
+    abel: 1
+  }
+  t.deepEqual(findMismatchDeep(pattern, actual), {
+    path: [ 'abba', 'bertil' ],
+    expected: [ { ceasar: [ { dustin: 99 } ] } ],
+    actual: [ { ceasar: [ { dustin: 66, eric: 4 } ], curt: 3 } ],
+  })
+  t.end()
+})
+
+test('array matcher (second mismatches)', t => {
+  const pattern = {
+    bertil: [
+      {
+        cedric: 2
+      },
+      {
+        ceasar: 3
+      }
+    ]
+  }
+  const actual = {
+    bertil: [
+      {
+        cedric: 2
+      },
+      {
+        ceasar: 4
+      }
+    ]
+  }
+  t.deepEqual(findMismatchDeep(pattern, actual), {
+    path: [ 'bertil' ],
+    expected: [ { cedric: 2 }, { ceasar: 3 } ],
+    actual: [ { cedric: 2 }, { ceasar: 4 } ]
+  })
+  t.end()
+})
